b2e3ffb78b758cb4255fea84223d141f8f0423fa
Eugene Bubnov <eugenebubnov@vk.com>
2025-11-04 09:19:21 +0300

    try to start

--- src/test/java/otus/RunnerTest.java
+++ src/test/java/otus/RunnerTest.java
@@ -2,15 +2,13 @@ package otus;
 
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
+import io.cucumber.junit.platform.engine.Cucumber;
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
 
-@Suite
-@IncludeEngines("cucumber")
-@SelectClasspathResource("scenarios")
-@ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "otus")
+@Cucumber
 public class RunnerTest {
 
 }
